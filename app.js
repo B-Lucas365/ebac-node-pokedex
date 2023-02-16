d3e7d758c68e3fb5c8bba6a398ae799ebdad061d
Lucas Renan <lucasrenan365@outlook.com>
2023-02-16 16:09:50 -0300

    instalada dependencias do projeto. nao consigo conectar ao banco

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
 const {connect} = require('./models')
 
 const pokemonsRouter = require('./routes/pokemons')
 
 const app = express()
+
+//configurando ejs
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 //declarando rotas
 app.use('/pokemons', pokemonsRouter)
